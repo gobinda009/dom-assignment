e228d2351995d980cd4981f8011e202e4049cd86
goibnda <gobindabauri008@gmail.com>
2024-01-23 15:34:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = document.getElementById("main");
+element.innerHTML=`Hello , I am a Div!`;
+element.style.backgroundColor = "yellow";
+element.style.color = "green";
+element.style.margin = "20px";
+element.style.padding = "10px";
+element.style.fontSize = "18px";
+element.style.fontWeight = "bold";
+element.style.height = "200px";
+element.style.width = "300px";
+
+
+
+
